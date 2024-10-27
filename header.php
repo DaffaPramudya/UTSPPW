ae682a2b601e3e3a0fbe8985ddf1c66ab98d53de
DaffaPramudya <daffapramudya160605@gmail.com>
2024-10-27 20:09:10 +0700

    Update logo profile button header

--- header.php
+++ header.php
@@ -1,20 +1,24 @@
     <!-- Header -->
     <header class="main-header">
         <div class="container">
-            <a href="index.php" class="main-logo"><h1 class="logo">Dalel Shop</h1></a>
+            <a href="index.php" class="main-logo">
+                <h1 class="logo">Dalel Shop</h1>
+            </a>
             <nav>
                 <ul>
                     <li><a href="login.php">Login</a></li>
                     <li><a href="register.php">Register</a></li>
                     <li>
-                        <input type="checkbox" id="menuToggle">
-                        <label for="menuToggle" class="menu-button">
-                            <img src="/assets/png/anonim.png" alt="Logout">
-                        </label>
-                        <div class="menu-dropdown">
-                            <ul>
-                                <li><a href="logout.php" style="color: blue; text-decoration: none;">Logout</a></li>
-                            </ul>
+                        <div class="main-header-profile-button">
+                            <input type="checkbox" id="menuToggle">
+                            <label for="menuToggle" class="menu-button">
+                                <i class="fa-solid fa-user"></i>
+                            </label>
+                            <div class="menu-dropdown">
+                                <ul>
+                                    <a href="logout.php" style="color: blue; text-decoration: none;">Logout</a>
+                                </ul>
+                            </div>
                         </div>
                     </li>
                 </ul>
