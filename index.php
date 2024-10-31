28a9e99f4b63525faf0f04687588a9242a617d40
DaffaPramudya <daffapramudya160605@gmail.com>
2024-10-31 20:19:14 +0700

    Ganti akun.php jadi profile.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php session_start(); 
-    include("database.php");
-    include("database2.php");
-    $res = all_table($conn, "produk");
+<?php session_start();
+include("database.php");
+include("database2.php");
+$res = all_table($conn, "produk");
 ?>
 
 <!DOCTYPE html>
@@ -27,12 +27,12 @@ include "head.php";
 
         <!-- Grid Produk -->
         <div class="product-grid">
-            <?php foreach ($res as $row) { 
+            <?php foreach ($res as $row) {
                 $harga = $row['hargaProduk'];
                 $harga = number_format($harga, 0, ',', '.');
-                ?>
+            ?>
                 <div class="product-item">
-                    <div class="product-image" style="background-color: #ddd; width: 100%; height: 150px;">
+                    <div class="product-image">
                         <img src="uploads/<?php echo $row['fotoProduk']; ?>" alt="">
                     </div>
                     <div class="product-name"><?php echo $row['namaProduk']; ?></div>
@@ -40,69 +40,6 @@ include "head.php";
                     <button class="buy-button">Add to cart</button>
                 </div>
             <?php } ?>
-            <!-- 
-            <div class="product-item">
-                <div class="product-image" style="background-color: #ddd; width: 100%; height: 150px;"></div>
-                <div class="product-name">Produk 2</div>
-                <div class="product-price">Rp 200.000</div>
-                <button class="buy-button">Beli</button>
-            </div>
-
-            <div class="product-item">
-                <div class="product-image" style="background-color: #ddd; width: 100%; height: 150px;"></div>
-                <div class="product-name">Produk 3</div>
-                <div class="product-price">Rp 150.000</div>
-                <button class="buy-button">Beli</button>
-            </div>
-
-            <div class="product-item">
-                <div class="product-image" style="background-color: #ddd; width: 100%; height: 150px;"></div>
-                <div class="product-name">Produk 4</div>
-                <div class="product-price">Rp 250.000</div>
-                <button class="buy-button">Beli</button>
-            </div>
-
-            <div class="product-item">
-                <div class="product-image" style="background-color: #ddd; width: 100%; height: 150px;"></div>
-                <div class="product-name">Produk 5</div>
-                <div class="product-price">Rp 120.000</div>
-                <button class="buy-button">Beli</button>
-            </div>
-
-            <div class="product-item">
-                <div class="product-image" style="background-color: #ddd; width: 100%; height: 150px;"></div>
-                <div class="product-name">Produk 6</div>
-                <div class="product-price">Rp 180.000</div>
-                <button class="buy-button">Beli</button>
-            </div>
-
-            <div class="product-item">
-                <div class="product-image" style="background-color: #ddd; width: 100%; height: 150px;"></div>
-                <div class="product-name">Produk 7</div>
-                <div class="product-price">Rp 300.000</div>
-                <button class="buy-button">Beli</button>
-            </div>
-
-            <div class="product-item">
-                <div class="product-image" style="background-color: #ddd; width: 100%; height: 150px;"></div>
-                <div class="product-name">Produk 8</div>
-                <div class="product-price">Rp 220.000</div>
-                <button class="buy-button">Beli</button>
-            </div>
-
-            <div class="product-item">
-                <div class="product-image" style="background-color: #ddd; width: 100%; height: 150px;"></div>
-                <div class="product-name">Produk 9</div>
-                <div class="product-price">Rp 320.000</div>
-                <button class="buy-button">Beli</button>
-            </div>
-
-            <div class="product-item">
-                <div class="product-image" style="background-color: #ddd; width: 100%; height: 150px;"></div>
-                <div class="product-name">Produk 10</div>
-                <div class="product-price">Rp 280.000</div>
-                <button class="buy-button">Beli</button>
-            </div>  -->
         </div>
 
     </div>
