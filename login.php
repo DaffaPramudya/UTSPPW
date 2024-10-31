0e7d999a46ad2ce5ff4525c08284376912ea6627
DaffaPramudya <daffapramudya160605@gmail.com>
2024-11-01 06:07:43 +0700

    Add $_SESSION['email'] after submit at login.php

--- login.php
+++ login.php
@@ -38,6 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['user_id'] = $row['id'];
             $_SESSION['username'] = $row['username'];
             $_SESSION['name'] = $row['name'];
+            $_SESSION['email'] = $row['email'];
 
             // Redirect ke halaman utama
             header("Location: index.php");
