e979aa27caa7354ea082710d34cf02651f0ed74e
AstericsDS <andhika2312@gmail.com>
2024-12-14 00:15:05 +0700

    improve responsiveness

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,74 +3,67 @@
 <x-head></x-head>
 <title>Dalel Shop - Login</title>
 
-<body class="bg-blue-600 mx-auto">
-    <div class="grid grid-cols-2 border rounded-lg mx-36 my-12 p-10 bg-white">
-        <div class="py-14 px-20 border shadow-lg rounded-lg ">
-            <form action="/login" method="post">
-                @csrf
-                <p class="text-2xl font-semibold mb-4 text-center">Login</p>
-                @if (session()->has('success'))
-                    <div class="success-message">
-                        {{ session('success') }}
-                    </div>
-                @endif
-                @if (session()->has('loginError'))
-                    <div class="error-message">
-                        {{ session('loginError') }}
-                    </div>
-                @endif
-                @if ($errors->any())
-                    <div class="error-message">
-                        @foreach ($errors->all() as $error)
-                            {{ $error }}
-                        @endforeach
-                    </div>
-                @endif
-                <!-- email -->
-                <div class="border px-2 py-1 mb-2 rounded-lg">
-                    <i class="fa-solid fa-user size-5"></i>
-                    <input class="p-1 text-sm focus:outline-none" type="email" name="email" placeholder="Email" value="{{ old('email') }}" required>
-                </div>
-
-                <!-- password -->
-                <div class="border px-2 py-1 rounded-lg">
-                    <i class="fa-solid fa-lock size-5"></i>
-                    <input  class="p-1 text-sm focus:outline-none" type="password" name="password" placeholder="Password" required>
-                </div>
-
-                <!-- google -->
-                <a href="" class="googleauth">
-                    <div class="flex items-center mt-6">
-                        <div class="flex-grow border-t border-gray-300"></div>
-                        <span class="px-2 text-gray-500 flex text-sm">or continue with </span>
-                        <div class="flex-grow border-t border-gray-300"></div>
-                    </div>
-
-                    <div class="flex justify-center mt-2 mb-4">
-                        <img src="{{ asset('svg/google.svg') }}" alt="" class="border rounded-lg px-16 py-2 text-center h-10">
-                        <!-- <i class="fa-brands fa-google border rounded-lg px-16 py-2  text-center"></i> -->
-                    </div>
-                </a>
-
-                <p id="captcha-question" class="text-sm mb-1">Berapakah hasil: {{session('captcha_num1')}} + {{session('captcha_num2')}}</p>
-                <div class="border rounded-lg px-2 py-1">
-                    <input type="text" name="captcha" placeholder="Hasil" class="focus:outline-none">
-                </div>
-
-                <div class="mt-4 mb-1 border rounded-lg w-40 justify-self-center py-2 flex items-center justify-center bg-blue-600 text-white font-semibold cursor-pointer">
-                    <input type="submit" value="Login" class="cursor-pointer">
-                </div>
-
-                
-            </form>
-            <p class="flex items-center justify-center text-sm">Belum punya akun? <a href="/register" class="ml-0.5 underline font-semibold"> Register</a></p>
+<body class="bg-auth-bg bg-cover min-h-screen min-w-screen flex items-center">
+  <div class="my-8 mx-auto md:grid md:grid-cols-2 border rounded-lg container px-6 py-10 sm:p-10 bg-white sm:block xl:max-w-screen-lg shadow-lg">
+    <div class="p-8 sm:py-14 sm:px-20 border shadow-lg rounded-lg md:p-8">
+      <form action="/login" method="post" class="flex flex-col mb-6">
+        @csrf
+        <p class="text-2xl font-semibold mb-4 text-center cursor-default">Login</p>
+        @if (session()->has('success'))
+          <div class="success-message">
+            {{ session('success') }}
+          </div>
+        @endif
+        @if (session()->has('loginError'))
+          <div class="bg-[#ffa07a] text-red-700 my-4 rounded-lg p-4">
+            {{ session('loginError') }}
+          </div>
+        @endif
+        @if ($errors->any())
+          <div class="error-message">
+            @foreach ($errors->all() as $error)
+              {{ $error }}
+            @endforeach
+          </div>
+        @endif
+        <!-- email -->
+        <div class="border-solid border py-3 px-3 mb-2 rounded-lg flex items-center overflow-hidden">
+          <i class="fa-solid fa-user w-4 h-4 mr-3"></i>
+          <input class="text-sm outline-none" type="email" name="email" placeholder="Email" value="{{ old('email') }}" required>
         </div>
 
-        <div class="relative justify-items-center items-center py-28 ">
-            <img src="svg/undraw_login_re_4vu2.svg" class="h-52 items-center">
+        <!-- password -->
+        <div class="border-solid border py-3 px-3 rounded-lg flex items-center overflow-hidden">
+          <i class="fa-solid fa-lock w-4 h-4 mr-3"></i>
+          <input class="text-sm outline-none" type="password" name="password" placeholder="Password" required>
         </div>
+
+        <!-- google -->
+        <div class="flex items-center mt-6">
+          <div class="flex-grow border-t border-gray-300"></div>
+          <span class="px-2 text-gray-500 flex text-sm cursor-default">or continue with </span>
+          <div class="flex-grow border-t border-gray-300"></div>
+        </div>
+        <div class="flex justify-center mt-2 mb-4">
+          <a href="#">
+            <img src="{{ asset('svg/google.svg') }}" alt="" class="border rounded-lg px-16 py-2 text-center h-10 hover:bg-slate-100 transition-colors">
+          </a>
+        </div>
+
+        <p id="captcha-question" class="text-sm mb-1">Berapakah hasil: {{ session('captcha_num1') }} + {{ session('captcha_num2') }}</p>
+        <input type="text" name="captcha" placeholder="Hasil" class="border rounded-lg p-2 outline-none text-sm">
+
+        <input type="submit" value="Login" class="mt-4 border rounded-lg w-40 py-2 self-center bg-blue-600 hover:bg-blue-700 transition-colors text-white font-semibold cursor-pointer">
+
+      </form>
+      <p class="flex items-center justify-center text-sm">Belum punya akun? <a href="/register" class="ml-0.5 underline font-semibold hover:text-gray-800 transition-colors"> Register</a></p>
     </div>
-    
+
+    <div class="hidden relative md:justify-center md:items-center md:flex md:p-8">
+      <img src="svg/undraw_login_re_4vu2.svg" class="h-52 items-center">
+    </div>
+  </div>
+
 </body>
 
 </html>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,57 +4,50 @@
 <x-head></x-head>
 <title>Dalel Shop - Register</title>
 
-<body class="bg-blue-600 mx-auto">
-    <div class="grid grid-cols-2 border rounded-lg mx-36 my-12 p-10 bg-white">
-        <div class="relative justify-items-center items-center py-28">
-            <img src="svg/undraw_join_re_w1lh.svg" class="h-64 items-center">
-        </div>
-
-        <div class="py-14 px-20 border shadow-lg rounded-lg">
-            <form action="/register" method="post">
-                @csrf
-                <p class="text-2xl font-semibold mb-8 text-center">Register</p>
-                <!-- Error message display -->
-                @if ($errors->any())
-                    <div class="error-message">
-                        @foreach ($errors->all() as $error)
-                            {{ $error }}
-                        @endforeach
-                    </div>
-                @endif
-
-                <!-- username -->
-                <div class="border px-2 py-1 mb-2 rounded-lg">
-                    <i class="fa-solid fa-user size-5"></i>
-                    <input class="p-1 text-sm focus:outline-none" type="text" name="username" placeholder="Username" value="{{ old('username') }}">
-                </div>
-
-                <!-- email -->
-                <div class="border px-2 py-1 mb-2 rounded-lg">
-                    <i class="fa-solid fa-envelope size-5"></i>
-                    <input class="p-1 text-sm focus:outline-none" type="email" name="email" placeholder="Email" value="{{ old('email') }}">
-                </div>
-
-                <!-- password -->
-                <div class="border px-2 py-1 rounded-lg mb-1">
-                    <i class="fa-solid fa-lock size-5"></i>
-                    <input class="p-1 text-sm focus:outline-none" type="password" name="password" placeholder="Password">
-                </div>
-
-                <div class="flex items-center gap-2 px-2">
-                    <input type="checkbox" name="isseller" id="isseller" value="1" class="cursor-pointer">
-                    <label for="isseller" class="text-sm cursor-pointer">Daftar sebagai seller</label>
-                </div>
-
-                <div class="mt-14 mb-1 border rounded-lg w-40 justify-self-center py-2 flex items-center justify-center bg-blue-600 text-white font-semibold cursor-pointer">
-                    <input type="submit" value="Register" class="cursor-pointer">
-                </div>
-                
-            </form>
-
-            <p class="flex items-center justify-center text-sm">Sudah punya akun? <a href="/login" class="ml-0.5 underline font-semibold">Login</a></p>
-        </div>
+<body class="bg-auth-bg bg-cover min-h-screen min-w-screen flex items-center">
+  <div class="my-8 mx-auto md:grid md:grid-cols-2 border rounded-lg container px-6 py-10 sm:p-10 bg-white sm:block xl:max-w-screen-lg shadow-lg">
+    <div class="hidden relative md:justify-center md:items-center md:flex">
+      <img src="svg/undraw_join_re_w1lh.svg" class="h-64 items-center">
     </div>
+
+    <div class="p-8 sm:py-14 sm:px-20 border shadow-lg rounded-lg md:p-8">
+      <form action="/register" method="post" class="flex flex-col mb-6">
+        @csrf
+        <p class="text-2xl font-semibold mb-8 text-center cursor-default">Register</p>
+        <!-- Error message display -->
+        @if ($errors->any())
+          <div class="error-message">
+            @foreach ($errors->all() as $error)
+              {{ $error }}
+            @endforeach
+          </div>
+        @endif
+
+        <!-- username -->
+        <div class="border-solid border py-3 px-3 mb-2 rounded-lg flex items-center overflow-hidden">
+          <i class="fa-solid fa-user w-4 h-4 mr-3"></i>
+          <input class="text-sm outline-none" type="text" name="username" placeholder="Username" value="{{ old('username') }}">
+        </div>
+
+        <!-- email -->
+        <div class="border-solid border py-3 px-3 mb-2 rounded-lg flex items-center overflow-hidden">
+          <i class="fa-solid fa-envelope w-4 h-4 mr-3"></i>
+          <input class="text-sm outline-none" type="email" name="email" placeholder="Email" value="{{ old('email') }}">
+        </div>
+
+        <!-- password -->
+        <div class="border-solid border py-3 px-3 mb-5 rounded-lg flex items-center overflow-hidden">
+          <i class="fa-solid fa-lock w-4 h-4 mr-3"></i>
+          <input class="text-sm outline-none" type="password" name="password" placeholder="Password">
+        </div>
+
+        <input type="submit" value="Register" class="border rounded-lg w-40 py-2 self-center bg-blue-600 hover:bg-blue-700 transition-colors text-white font-semibold cursor-pointer">
+
+      </form>
+
+      <p class="flex items-center justify-center text-sm">Sudah punya akun? <a href="/login" class="ml-0.5 underline font-semibold hover:text-gray-800 transition-colors">Login</a></p>
+    </div>
+  </div>
 </body>
 
 </html>
